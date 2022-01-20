7af153af86fd1fa18fa4f10e3e2c61810f327ba4
Hansika007 <95028012+Hansika007@users.noreply.github.com>
2022-01-20 18:12:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+var images = ["https://i.postimg.cc/MGn9GJXw/family.jpg","https://i.postimg.cc/qqyYvVbq/grandpa.jpg", "https://i.postimg.cc/wjMnFtMX/father.jpg" , "https://i.postimg.cc/5ymDKL83/bro.jpg", "IMG_20141006_133559.jpg", "IMG_20190612_170114.jpg"];
+var names = ["Fmaily Book","Shri Bhagwan", "Mr. Parveen Kumar", "Tarun", "Hnsika", "Mrs. Sudesh Kumari"];
+var i = 0;
+function update()
+{
+    i++;
+    var numbers_of_family_member_in_array = 5
+    if(i > numbers_of_family_member_in_array)
+      {
+          i = 0;
+      }
+    
+    //Debug the code to store list of images in updatedImage. Use images[i]
+    var updatedImage = images[i];
+    //Debug the code to store list of names in updatedName. Use names[i]
+    var updatedName = names[i] ;
+ 
+    document.getElementById("family_member_image").src = updatedImage;
+    document.getElementById("family_member_name").innerHTML = updatedName;
+}
